a57660a2df662381db3d90fc564e98cec65d0908
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-20 21:52:00 +0100

    Add Settings View Controller and Tab Bar Controller

---
+++ Breakout/Breakout/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  Breakout
+//
+//  Created by Sebastian Osiński on 20.12.2015.
+//  Copyright © 2015 Sebastian Osiński. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
