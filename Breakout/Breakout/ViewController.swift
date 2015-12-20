216c1952b30d6c7d85b58fc4c400e9b07397f170
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-20 21:41:31 +0100

    Breakout init

---
+++ Breakout/Breakout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Breakout
+//
+//  Created by Sebastian Osiński on 20.12.2015.
+//  Copyright © 2015 Sebastian Osiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
