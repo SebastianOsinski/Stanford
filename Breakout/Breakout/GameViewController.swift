bb700c7be8efefdd470d28eb40ab21f366122465
Sebastian Osiński <seb.osinski@gmail.com>
2015-12-20 23:20:10 +0100

    Add paddle and pan gesture for moving paddle

--- Breakout/Breakout/GameViewController.swift
+++ Breakout/Breakout/GameViewController.swift
@@ -13,16 +13,23 @@ class GameViewController: UIViewController {
     var bricksPerRow: Int = 5
     var numberOfRows: Int = 4
     
+    var paddleWidthRatio: CGFloat = 0.4
+    var paddle: UIView?
+    var paddleGestureRecogniser: UIPanGestureRecognizer!
+    
     @IBOutlet weak var gameView: UIView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        paddleGestureRecogniser = UIPanGestureRecognizer(target: self, action: "paddleMoved:")
     }
     
     override func viewDidLayoutSubviews() {
         gameView.subviews.forEach { $0.removeFromSuperview() }
         
         let width = gameView.bounds.width
+        let height = gameView.bounds.height
+        
         let brickSpacing = width * 0.025
         let brickWidth = (width - brickSpacing * CGFloat(bricksPerRow + 1)) / CGFloat(bricksPerRow)
         let brickHeight: CGFloat = 20
@@ -41,7 +48,25 @@ class GameViewController: UIViewController {
             }
         }
         
+        let paddleOrigin = CGPoint(x: width * (1 - paddleWidthRatio) / 2, y: height * 0.9)
+        let paddleSize = CGSize(width: width * paddleWidthRatio, height: height * 0.05)
+        let paddleRect = CGRect(origin: paddleOrigin, size: paddleSize)
+        
+        paddle = UIView(frame: paddleRect)
+        paddle!.backgroundColor = UIColor.redColor()
+        paddle!.addGestureRecognizer(paddleGestureRecogniser)
+        gameView.addSubview(paddle!)
     }
 
+   func paddleMoved(sender: UIPanGestureRecognizer) {
+        if sender.state == .Changed || sender.state == .Ended {
+            let translation = sender.translationInView(gameView)
+            
+            if let paddleView = sender.view {
+                paddleView.center = CGPointMake(paddleView.center.x + translation.x, paddleView.center.y)
+                sender.setTranslation(CGPoint.zero, inView: paddleView)
+            }
+        }
+    }
 }
 
